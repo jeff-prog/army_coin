cdee517588488c24df6786ed39498941d08f91f4
jeff-prog <54414275+jeff-prog@users.noreply.github.com>
2024-10-21 12:44:52 -0300

    Add files via upload

--- bot_jogo.py
+++ bot_jogo.py
@@ -5,7 +5,7 @@ async def start(update: Update, context: CallbackContext) -> None:
     keyboard = [
         [
             InlineKeyboardButton(
-                "Jogar", web_app=WebAppInfo(url="https://army-coin-831q.vercel.app/")  # Coloque aqui o link do seu jogo
+                "Jogar", web_app=WebAppInfo(url="https://army-coin-ux1h.vercel.app/")  # Coloque aqui o link do seu jogo
             )
         ]
     ]
